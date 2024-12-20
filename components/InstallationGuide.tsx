902a543d5b21b9cec7ccde88ec632190aed6d143
sound:1 <sound1@pizza-bot.com>
2024-12-20 15:38:29 +0100

    Update InstallationGuide.tsx

--- components/InstallationGuide.tsx
+++ components/InstallationGuide.tsx
@@ -10,7 +10,7 @@ export function InstallationGuide() {
       <ol className="list-decimal pl-8 space-y-4 text-[#E5E5E5]">
         <li>Download the latest <code className="bg-[#111111] px-2 py-1 rounded font-mono">Borion.dll</code> from the button above.</li>
         <li>Ensure you have Minecraft Bedrock Edition installed and running.</li>
-        <li>Use a dll loader like <a href="https://github.com/fligger/FateInjector" className="text-blue-400 hover:text-blue-300">fateinjector</a>, <a href="https://xealah.com/" className="text-blue-400 hover:text-blue-300">modinjector</a>, or the <a href="https://horion.download/" className="text-blue-400 hover:text-blue-300">Horion one</a> by right-clicking "inject"</li>
+        <li>Use a dll loader like <a href="https://github.com/fligger/FateInjector" className="text-blue-400 hover:text-blue-300">FateInjector</a> or <a href="https://horion.download/" className="text-blue-400 hover:text-blue-300">Horion Injector</a> by right-clicking "inject"</li>
         <li>Enjoy!</li>
       </ol>
       <p className="text-center text-red-400 mt-8 bg-[#111111] py-2 px-4 rounded-md inline-block">
